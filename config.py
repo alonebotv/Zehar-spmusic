47e3d8239419648ea688cb5462176d2787cb30d8
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2023-10-24 03:05:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ API_HASH = getenv("API_HASH","b463bedd791aa733ae2297e6520302fe")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_USERNAME = getenv("OWNER_USERNAME","AM_YTBOTT")
+ABOUT_ME = getenv("ABOUT_ME","https://t.me/About_AMBot")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://AbhiModszYT:AbhiModszYT@abhimodszyt.pom3ops.mongodb.net/?retryWrites=true&w=majority")
 
